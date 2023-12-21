13a68e025cc8f5b84865f8afd1d3dcfb6d7f1cca
william-rauwens-oliver <william@PcWIlly>
2023-12-21 15:44:50 +0100

    add comentary

--- ia.py
+++ ia.py
@@ -6,10 +6,10 @@ class IA:
         self.directions = [Vector2(1, 0), Vector2(-1, 0), Vector2(0, 1), Vector2(0, -1)]
 
     def get_next_move(self, snake, fruit):
-        # Obtenez la direction de la tête du serpent vers la pomme
+        # Direction de la tête du serpent vers la pomme
         to_fruit = fruit.pos - snake.body[0]
 
-        # Choisissez la direction en fonction de la position relative de la pomme
+        # Direction en fonction de la position relative de la pomme
         if to_fruit.x > 0:
             next_direction = Vector2(1, 0)
         elif to_fruit.x < 0:
@@ -19,7 +19,7 @@ class IA:
         elif to_fruit.y < 0:
             next_direction = Vector2(0, -1)
         else:
-            # Si la tête est déjà sur la pomme, choisissez une direction aléatoire
+            # Si la tête est déjà sur la pomme, elle choisie une direction aléatoire
             next_direction = random.choice(self.directions)
 
         return next_direction
