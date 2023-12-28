bbc4721ae323e990184af2ca06d0de9f705b9981
william-rauwens-oliver <william@PcWIlly>
2023-12-28 14:54:39 +0100

    update scores

--- snake.py
+++ snake.py
@@ -196,7 +196,8 @@ class MainGame:
         self.snake = Snake()
         self.fruit = Fruit()
         self.mode = mode
-        self.score = 0  # Nouvelle variable pour le score
+        self.score = 0
+        self.score_recorded = False  # Nouveau booléen pour suivre si le score a été enregistré
         if self.mode == "IA":
             self.ai = IA()
 
@@ -219,7 +220,11 @@ class MainGame:
             self.fruit.randomize()
             self.snake.add_block()
             self.snake.play_crunch_sound()
-            self.score += 1  # Incrémente le score
+            self.score += 1
+
+            if self.mode == "Solo" and not self.score_recorded:
+                self.save_score()
+                self.score_recorded = True
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
@@ -231,7 +236,8 @@ class MainGame:
 
     def game_over(self):
         self.snake.reset()
-        self.save_score()  # Enregistre le score dans le fichier "scores.txt"
+        self.save_score()
+        self.score_recorded = False  # Réinitialise le booléen lorsque le jeu est réinitialisé
 
     def save_score(self):
         with open('scores.txt', 'a') as file:
@@ -252,20 +258,20 @@ class MainGame:
                         pygame.draw.rect(screen, grass_color, grass_rect)
 
     def draw_score(self):
-        score_text = str(len(self.snake.body) - 3)
+        score_text = str(self.score)
         score_surface = game_font.render(score_text, True, (56, 74, 12))
         score_x = int(cell_size * cell_number - 60)
         score_y = int(cell_size * cell_number - 40)
         score_rect = score_surface.get_rect(center=(score_x, score_y))
         apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
         bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6,
-                              apple_rect.height)
+                            apple_rect.height)
 
         pygame.draw.rect(screen, (167, 209, 61), bg_rect)
         screen.blit(score_surface, score_rect)
         screen.blit(apple, apple_rect)
         pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
-
+        
 class ScoreScreen:
     def __init__(self):
         self.font = pygame.font.Font(None, 36)
@@ -288,7 +294,7 @@ class ScoreScreen:
         return scores
 
     def draw_scores(self, scores):
-        screen.fill((255, 255, 255))  # Fond blanc
+        screen.fill((255, 255, 255))
 
         # Titre
         title_text = self.title_font.render("Scores", True, (0, 0, 0))
